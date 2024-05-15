287bb8d470b05947160e67e7e2fcbc237daf9b9c
misbahsiddiqq <misbah.dhanani@gmail.com>
2024-05-16 00:27:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// import inquirer from "enquirer";
+// //declare a constant "answer" and asssign it to the result of inquire.prompt function
+// const answer: {
+//     sentence: "string",
+// } = await inquirer.prompt([
+//   {
+//     type: "input",
+//     name: "Sentence",
+//     message: "Enter your sentence to count the word?"
+//   }
+// ]);
+// const words = answer.sentence.trim().split(" ")
+// //printing the array of words
+// console.log(words)
+// //print the word count of the sentence to consol
+// console.log(`"Your sentence word count is ${words.length}"`);
